27ab9d542b773d53126c54cbc67053caa2a8adf1
Maria Melnychuk <mv.melnyc2@gmail.com>
2025-09-02 15:14:48 +0300

    day 2 task

--- lib/day_2/task.rb
+++ lib/day_2/task.rb
@@ -5,13 +5,37 @@ module Day2
     end
 
     def call1
+      input.count do |line|
+        valid_report?(line)
+      end
     end
 
     def call2
+      input.count do |line|
+        valid_report?(line) || single_bad_level?(line)
+      end
     end
 
     def input
       @input ||= Input.call(@sample)
+    end
+
+    private
+
+    def valid_report?(line)
+      diff = calculate_diff(line)
+
+      (diff.all?(&:positive?) || diff.all?(&:negative?)) && diff.all? { |r| r.abs <= 3 }
+    end
+
+    def calculate_diff(line)
+      line.each_cons(2).map { |a, b| a - b }
+    end
+
+    def single_bad_level?(line)
+      [*0...line.length].any? do |i|
+        valid_report?(line[0...i] + line[i + 1..])
+      end
     end
   end
 end

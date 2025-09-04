2a41890ec93fa4956a8ff5b06ed7463f51a7e097
Maria Melnychuk <mv.melnyc2@gmail.com>
2025-09-04 18:18:36 +0300

    finish third task

--- lib/day_3/task.rb
+++ lib/day_3/task.rb
@@ -6,12 +6,27 @@ module Day3
       @sample = sample
     end
 
-    def call1; end
+    def call1
+      sum_products(input)
+    end
 
-    def call2; end
+    def call2
+      validated_input = filtered_commands_input(input)
+      sum_products(validated_input)
+    end
 
     def input
       @input ||= Input.call(@sample)
+    end
+
+    private
+
+    def sum_products(input)
+      input.scan(/mul\((\d{1,3}),(\d{1,3})\)/).sum { |a, b| a.to_i * b.to_i }
+    end
+
+    def filtered_commands_input(input)
+      input.gsub(/don't\(\).*?do\(\)/m, '')
     end
   end
 end

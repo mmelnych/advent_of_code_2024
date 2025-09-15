ca039e90df66b1df924c4efd2c3bf4749a9820b4
Maria Melnychuk <mv.melnyc2@gmail.com>
2025-09-15 15:26:04 +0300

    small refactoring

--- lib/day_7/task.rb
+++ lib/day_7/task.rb
@@ -2,44 +2,41 @@
 
 module Day7
   class Task
+    OPERATORS_FOR_PART1 = ['+', '*'].freeze
+    OPERATORS_FOR_PART2 = ['+', '*', '||'].freeze
+
     def initialize(sample)
       @sample = sample
     end
 
     def call1
-      result = input.select do |entry|
-        operands = ['+', '*'].repeated_permutation(entry[:numbers].size - 1).to_a
-        operands.any? do |ops|
-          sum = entry[:numbers].first
-          entry[:numbers][1..].each_with_index do |num, idx|
-            sum = sum.send(ops[idx] == '+' ? :+ : :*, num)
-          end
-          sum == entry[:result]
-        end
-      end
-      result.sum { |x| x[:result] }
+      input.select { |entry| valid_expression?(entry, OPERATORS_FOR_PART1) }.sum { |x| x[:result] }
     end
 
     def call2
-      result = input.select do |entry|
-        operands = ['+', '*', '||'].repeated_permutation(entry[:numbers].size - 1).to_a
-        operands.any? do |ops|
-          sum = entry[:numbers].first
-          entry[:numbers][1..].each_with_index do |num, idx|
-            sum = if ops[idx] == '||'
-                    "#{sum}#{num}".to_i
-                  else
-                    sum.send(ops[idx] == '+' ? :+ : :*, num)
-                  end
-          end
-          sum == entry[:result]
-        end
-      end
-      result.sum { |x| x[:result] }
+      input.select { |entry| valid_expression?(entry, OPERATORS_FOR_PART2) }.sum { |x| x[:result] }
     end
+
+    private
 
     def input
       @input ||= Input.call(@sample)
+    end
+
+    def valid_expression?(entry, operators)
+      combinations = operators.repeated_permutation(entry[:numbers].size - 1).to_a
+
+      combinations.any? do |ops|
+        sum = entry[:numbers].first
+        entry[:numbers][1..].each_with_index do |num, idx|
+          sum = if ops[idx] == '||'
+                  "#{sum}#{num}".to_i
+                else
+                  sum.send(ops[idx] == '+' ? :+ : :*, num)
+                end
+        end
+        sum == entry[:result]
+      end
     end
   end
 end
